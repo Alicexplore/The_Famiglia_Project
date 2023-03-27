b2e3ec7509a33aea3005cc31ef76f008e05c5482
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-03-27 23:56:18 +0200

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='mx-auto '>
-        <div className='max-h-[280px] bg-black relative'>
-            <div className='absolute w-full h-full text-[#F2F2F2] max-h-[280px] bg-[#151515]'>
+        <div className='h-[1px] bg-[#6f6f6f]'></div>
+        <div className='h-[210px] bg-[#151515] relative'>
+            <div className='absolute w-full text-[#F2F2F2] h-[210px] bg-[#151515]'>
                 <div className='flex flex-row flex-nowrap gap-40'>
                     <div className='flex flex-col'>
                         <div className='px-20 py-6'>
@@ -13,13 +14,6 @@ const Footer = () => {
                             <h2 className='font-worksans text-white text-sm font-thin py-1'>+1 212-719-1300</h2>
                             <h2 className='font-worksans text-white text-sm font-thin -py-1 tracking-widest'>Famiglia@gmail.com</h2>
                         </div> 
-                        <div className='flex row py-2 px-20 gap-1'>
-                            <button className='-ml-6'><img src="./social/social_b-01.svg" alt="/" width={33}/></button>
-                            <button className='-ml-8'><img src="./social/social_b-02.svg" alt="/" width={33}/></button>
-                            <button className='-ml-8'><img src="./social/social_b-04.svg" alt="/" width={33}/></button>
-                            <button className='-ml-8'><img src="./social/social_b-05.svg" alt="/" width={33}/></button>
-                            <button className='-ml-8'><img src="./social/social_b-06.svg" alt="/" width={33}/></button>
-                        </div>  
                     </div>
                     <div className='flex py-10 px-4 font-work-sans font-base'>    
                         <h1>USEFUL LINKS</h1>
@@ -30,16 +24,19 @@ const Footer = () => {
                         
                     </div>
                     <div className='flex py-10 px-4'>    
-                        <h1 className='flex-start'>USEFUL LINKS</h1>
-                       
+                            <button className='-ml-6'><img src="./social/social_b-01.svg" alt="/" width={33}/></button>
+                            <button className='-ml-8'><img src="./social/social_b-02.svg" alt="/" width={33}/></button>
+                            <button className='-ml-8'><img src="./social/social_b-04.svg" alt="/" width={33}/></button>
+                            <button className='-ml-8'><img src="./social/social_b-05.svg" alt="/" width={33}/></button>
+                            <button className='-ml-8'><img src="./social/social_b-06.svg" alt="/" width={33}/></button>  
                     </div>
                 
                 
                     
                 </div>         
             </div>    
-            <img className='object-cover h-[280px] w-full' src="./food/jodie-morgan-v25z8P-CPB4-unsplash.jpg" alt="vegetables"/>
         </div>
+        <div className='h-[1px] bg-[#6f6f6f]'></div>
     </div>
   )
 }
