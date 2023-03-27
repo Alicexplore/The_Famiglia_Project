b3534d2ef51728fd3bc570857418379eb3bc1e04
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-03-28 01:46:11 +0200

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,37 +13,40 @@ const Footer = () => {
                     <h2 className='font-worksans text-white text-sm font-thin py-1'>+1 212-719-1300</h2>
                     <h2 className='font-worksans text-white text-sm font-thin -py-1 tracking-widest'>Famiglia@gmail.com</h2>
                 </div> 
-                <div className='grid grid-rows-6 '>    
+                <div className='grid grid-rows-6 text-center'>    
                     <h1>USEFUL LINKS</h1>
-                    
                         <h6 className='text-sm font-thin'>Lorem ipsum</h6>
                         <h6 className='text-sm font-thin'>Lorem ipsum</h6>
                         <h6 className='text-sm font-thin'>Lorem ipsum</h6>
                         <h6 className='text-sm font-thin'>Lorem ipsum</h6>
-                      
-                   
                 </div>
-                    <div className=''>    
-                        <h1>USEFUL LINKS</h1>
-                        
+                <div className='grid grid-rows-6 text-center'>    
+                    <h1>USEFUL LINKS</h1>
+                        <h6 className='text-sm font-thin'>Lorem ipsum</h6>
+                        <h6 className='text-sm font-thin'>Lorem ipsum</h6>
+                        <h6 className='text-sm font-thin'>Lorem ipsum</h6>
+                        <h6 className='text-sm font-thin'>Lorem ipsum</h6>
+                </div>
+                <div className='grid grid-rows-6 text-center'>    
+                    <h1>USEFUL LINKS</h1>
+                        <h6 className='text-sm font-thin'>Lorem ipsum</h6>
+                        <h6 className='text-sm font-thin'>Lorem ipsum</h6>
+                        <h6 className='text-sm font-thin'>Lorem ipsum</h6>
+                        <h6 className='text-sm font-thin'>Lorem ipsum</h6>
+                </div>
+                <div className='grid grid-rows-2 place-items-center -space-y-16'>  
+                    <div className='grid grid-cols-2'>
+                        <input className='border p-2 text-black focus:outline-none focus:border-black focus:ring-1 focus:ring-white' type="password" placeholder='Newsletter'/>
+                        <button className=''>GO</button>
+                    </div>  
+                    <div className='grid grid-cols-5 gap-4'>
+                        <a href='/' className=''><img src="./social/social_b-01.svg" alt="/" width={40}/></a>
+                        <a href='/' className=''><img src="./social/social_b-02.svg" alt="/" width={40}/></a>
+                        <a href='/' className=''><img src="./social/social_b-04.svg" alt="/" width={40}/></a>
+                        <a href='/' className=''><img src="./social/social_b-05.svg" alt="/" width={40}/></a>
+                        <a href='/' className=''><img src="./social/social_b-06.svg" alt="/" width={40}/></a>  
                     </div>
-                    <div className=''>    
-                        <h1>USEFUL LINKS</h1>
-                        
-                    </div>
-                    <div className='grid grid-rows-2 '>  
-                        <div className='grid grid-cols-2'>
-                            <input className='border p-2 text-black focus:outline-none focus:border-black focus:ring-1 focus:ring-white' type="password" placeholder='Newsletter'/>
-                            <button className=''>GO</button>
-                        </div>  
-                        <div className='grid grid-cols-5'>
-                            <button className=''><img src="./social/social_b-01.svg" alt="/" width={33}/></button>
-                            <button className=''><img src="./social/social_b-02.svg" alt="/" width={33}/></button>
-                            <button className=''><img src="./social/social_b-04.svg" alt="/" width={33}/></button>
-                            <button className=''><img src="./social/social_b-05.svg" alt="/" width={33}/></button>
-                            <button className=''><img src="./social/social_b-06.svg" alt="/" width={33}/></button>  
-                        </div>
-                    </div>
+                </div>
                 
                 
                     
